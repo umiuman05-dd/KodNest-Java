6d46d1b21f176078e9eb94f6b0f89bee8e57a6a2
umiuman05-dd <umiuman05@gmail.com>
2026-07-30 13:08:07 +0530

    Day1 Task

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -5,7 +5,7 @@ public class WelcomeToJava {
         System.out.println("Welcome to the java track");
         System.out.println("I already know how to build programmmig logic");
         System.out.println("Now I am learning how to express that logic using java");
-        System.out.println("Now i know how to use git hub");
+        System.out.println("Now i know how to use git and git hub");
 
     }
 }
