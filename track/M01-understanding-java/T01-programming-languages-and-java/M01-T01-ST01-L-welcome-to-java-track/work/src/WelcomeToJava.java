cc3311fe638922c3881b59a6cd79b5043372bae2
umiuman05-dd <umiuman05@gmail.com>
2026-07-30 12:56:50 +0530

    Day1changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,9 +1,10 @@
-public class WelcomeToJava {
-    public static void main(String[] args) {
-    System.out.println("Welcome to the java track");
-    System.out.println("I already know how to build programmmig logic");
-    System.out.println("Now I am learning how to express that logic using java");
 
+public class WelcomeToJava {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the java track");
+        System.out.println("I already know how to build programmmig logic");
+        System.out.println("Now I am learning how to express that logic using java");
 
     }
 }
