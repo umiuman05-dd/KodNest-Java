5ba83ec8819ccfe319c3693f77e6a83b0a3a3528
umiuman05-dd <umiuman05@gmail.com>
2026-08-07 11:56:32 +0530

    nextline

---
+++ track/M02/T01/ST01/UserInput2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class UserInput2{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your age : ");
+        int age = scan.nextInt();
+        System.out.println("Age is" + age);
+        System.out.println("Enter you height :");
+        float height = scan.nextFloat();
+        System.out.println("your height : " + height);
+        scan.nextLine();
+        System.out.println("Enter your full name :");
+        String fname = scan.nextLine();
+        System.out.println("Full name is : " + fname);
+    }
+}
