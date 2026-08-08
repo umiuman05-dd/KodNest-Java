23f43895d98d0c22ad0f1d74cbd9bfd61a58b64f
umiuman05-dd <umiuman05@gmail.com>
2026-08-08 21:09:36 +0530

    placement eligibility checker

--- track/M02/T01/ST01/placement.java
+++ track/M02/T01/ST01/placement.java
@@ -1,18 +1,13 @@
 public class placement {
     public static void main(String[] args) {
-        // write your code here
-        int CompletedTopics = 17;
-        int totalTopics = 20;
-        int DailyLearningHours = 3;
-        int LearningDays = 5;
-        
-        int remainingTopics = totalTopics - CompletedTopics;
-        int weeklyLearningHours = DailyLearningHours * LearningDays;
-        double progressPercentage = (double) CompletedTopics * 100 / totalTopics;
-        
-        System.out.println("Completed Topics: " + CompletedTopics);
-        System.out.println("Remaining Topics: " + remainingTopics);
-        System.out.println("Weekly Learning Hours: " + weeklyLearningHours);
-        System.out.println("Progress Percentage: " + progressPercentage);
+        int marks = 72;
+        int attendance = 80;
+        boolean projectCompleted = true;
+
+        boolean marksEligible = marks >= 60;
+        boolean attendanceEligible = attendance >= 75;
+        boolean academicEligible = marksEligible && attendanceEligible;
+        boolean placementEligible = academicEligible && projectCompleted;
+        System.out.println("Placement Eligible: " + placementEligible);
     }
 }
