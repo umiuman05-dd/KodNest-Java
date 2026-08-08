fa86adace46c75734082ee4bf79d0f84dfcf486e
umiuman05-dd <umiuman05@gmail.com>
2026-08-08 22:17:53 +0530

    learner  profile

---
+++ track/M02/T01/ST01/studentPro.java
@@ -0,0 +1,14 @@
+public class studentPro {
+    public static void main(String[] args) {
+        // display student profile
+        String learnerName = "Ananya";
+        int completedProblems = 25;
+        double practiceHours = 2.5;
+        boolean projectCompleted = true;
+        
+        System.out.println("Learner: " + learnerName);
+        System.out.println("Problems Completed: " + completedProblems);
+        System.out.println("Practice Hours: " + practiceHours);
+        System.out.println("Project Completed: " + projectCompleted);
+    }
+}
