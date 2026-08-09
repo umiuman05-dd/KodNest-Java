e3eddbce683f387a2cf75bcc58ff57159a9046a5
umiuman05-dd <umiuman05@gmail.com>
2026-08-09 11:58:30 +0530

    grouping &&

---
+++ track/M02/T01/ST01/groupingJT.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class groupingJT {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int codingScore = scanner.nextInt();
+        int attendance = scanner.nextInt();
+        int projects = scanner.nextInt();
+
+        if (codingScore >= 60 && (attendance >= 75 || projects >= 2)) {
+            System.out.println("Eligible");
+        } else {
+            System.out.println("Not Eligible");
+        }
+
+        scanner.close();
+    }
+}
