85239632ac0d923426b4308a4a1b50458816ac78
umiuman05-dd <umiuman05@gmail.com>
2026-08-10 13:07:50 +0530

    class and object

---
+++ track/M02/T01/ST01/StudentApplication.java
@@ -0,0 +1,30 @@
+class Student
+{
+    int roll;
+    String name;
+    double height;
+    void run()
+    {
+        System.out.println("running");
+    }
+    void sleep()
+    {
+        System.out.println("sleeping");
+    }
+}
+class StudentApplication
+{
+    public static void main(String[] args)
+    {
+        Student s1 = new Student();
+        s1.roll=12;
+        s1.name="Raja";
+        s1.height=5.5;
+        
+        System.out.println(s1.roll);
+        System.out.println(s1.name);
+        System.out.println(s1.height);
+        s1.run();
+        s1.sleep();
+    }
+}
